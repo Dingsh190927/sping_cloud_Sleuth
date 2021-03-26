880b6556ab83c03897b6d34158fa901509ceb520
王凯 <88616879@qq.com>
2021-03-26 09:54:18 +0800

    服务链路追踪

---
+++ eureka-server/src/test/java/com/dforesh/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dforesh.eurekaserver;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+
+    void contextLoads() {
+    }
+
+}
